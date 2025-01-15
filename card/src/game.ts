27df1cad543826e981940bd97bf37b86c48bb7ae
re2osushi8888 <re.2osushi8888@gmail.com>
2025-01-15 12:51:29 +0000

    アクティブプレイヤーの交代ができる

--- card/src/game.ts
+++ card/src/game.ts
@@ -1,7 +1,20 @@
 export class Game {
+	isActivePlayer(): string {
+		return this.activePlayer;
+	}
+
+	turnChange(): void {
+		if (this.activePlayer === this.player1) {
+			this.activePlayer = this.player2;
+		} else {
+			this.activePlayer = this.player1;
+		}
+	}
+
 	constructor(
 		private player1: string,
 		private player2: string,
+		private activePlayer: string = player1,
 	) {}
 
 	getWinner(): string {
