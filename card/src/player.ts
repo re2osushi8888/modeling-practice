0316eee5b3ccfa40c1f1239590f992e11faf3302
re2osushi8888 <re.2osushi8888@gmail.com>
2025-01-29 12:58:08 +0000

    Playerのフィールドをprivate化する

--- card/src/player.ts
+++ card/src/player.ts
@@ -1,13 +1,22 @@
 export class Player {
 	constructor(
-		public name: string,
-		public life = 1,
+		private name: string,
+		private life = 1,
 		private mana = 0,
 	) {}
 	manaCharge(): void {
 		this.mana += 1;
 	}
+	takeDamage(damage: number): void {
+		this.life -= damage;
+	}
 	getMana(): number {
 		return this.mana;
+	}
+	getName(): string {
+		return this.name;
+	}
+	getLife(): number {
+		return this.life;
 	}
 }
