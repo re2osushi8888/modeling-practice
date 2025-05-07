ac8f412a29578e1f8592ee292382b495b1d85d77
re2osushi8888 <re.2osushi8888@gmail.com>
2025-05-07 12:50:36 +0000

    ゲームの勝者が決まる
    Co-authored-by: aki-moon <aki-moon@users.noreply.github.com>

---
+++ card-func-style/src/game.ts
@@ -0,0 +1,8 @@
+export type GameStatus = {
+  player1: string,
+  player2: string
+}
+
+export const winner = (game: GameStatus) => {
+  return 'れつ'
+}
--- card-func-style/tests/index.test.ts
+++ card-func-style/tests/index.test.ts
@@ -1,5 +1,11 @@
 import { expect, test } from 'bun:test';
+import { winner, type GameStatus } from '../src/game';
 
-test('init test', () => {
-	expect(true).toBeTrue();
-});
+test('ゲームの勝者が決まる', () => {
+  const gameStatus: GameStatus = {
+    player1: 'れつ',
+    player2: 'おおくら'
+  }
+
+  expect(winner(gameStatus)).toBe('れつ')
+})
