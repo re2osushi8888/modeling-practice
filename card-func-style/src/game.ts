68ab835a3b87cc4415f9fd5d69266454a08609a9
re2osushi8888 <re.2osushi8888@gmail.com>
2025-05-14 12:49:52 +0000

    format

--- card-func-style/src/game.ts
+++ card-func-style/src/game.ts
@@ -1,10 +1,10 @@
 export type GameStatus = {
-  player1: string,
-  player2: string,
-  activePlayer: string,
-  opponentPlayer: string
-}
+	player1: string;
+	player2: string;
+	activePlayer: string;
+	opponentPlayer: string;
+};
 
 export const winner = (game: GameStatus) => {
-  return game.activePlayer
-}
+	return game.activePlayer;
+};
