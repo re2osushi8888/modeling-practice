4815802360ae7b8a72e7cf526fd083519043e9aa
re2osushi8888 <re.2osushi8888@gmail.com>
2025-06-01 04:16:06 +0000

    addのテスト

--- learn-hs/test/Spec.hs
+++ learn-hs/test/Spec.hs
@@ -1,7 +1,13 @@
 import Test.Hspec
 
+add :: Num a => a -> a -> a
+add x y = x + y
+
 main :: IO ()
+
 main = hspec $ do
   describe "Example test" $ do
-    it "should pass" $ do
-      (1 + 1) `shouldBe` 2
+    it "1 + 1 = 2" $ do
+      add 1 1 `shouldBe` 2
+    it "2 + 2 = 4" $ do
+      (2 + 2) `shouldBe` 4
