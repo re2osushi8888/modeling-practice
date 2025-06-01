12c8aae71955b3f0ce7a6c9d3ee72177e3dce6fa
re2osushi8888 <re.2osushi8888@gmail.com>
2025-06-01 04:35:27 +0000

    四則演算を試す

--- learn-hs/src/Add.hs
+++ learn-hs/src/Add.hs
@@ -5,3 +5,11 @@ main :: IO ()
 main = do
     print $ add 1 2 
     print $ 1 `add` 2
+    print $ 1 + 2
+    print $ (+) 1 2
+    print $ 5 + 2
+    print $ 5 - 2
+    print $ 5 * 2
+    print $ 5 / 2
+    print $ div 5 2 --割った時の商の数
+    print $ mod 5 2 --割った時の剰余（あまりの数））
