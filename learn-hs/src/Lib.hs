5c83c5d6b32437d71f1ce7c16ddbf253e963348c
re2osushi8888 <re.2osushi8888@gmail.com>
2025-06-11 13:00:24 +0000

    fizzBuzzのfizzまで実装

--- learn-hs/src/Lib.hs
+++ learn-hs/src/Lib.hs
@@ -9,3 +9,4 @@ someFunc = putStrLn "someFunc"
 
 add :: (Num a) => a -> a -> a
 add x y = x + y
+
--- learn-hs/test/Spec.hs
+++ learn-hs/test/Spec.hs
@@ -3,6 +3,9 @@
 import Lib (add)
 import Test.Hspec
 
+fizzBuzz :: Integer -> String
+fizzBuzz x = if mod x 3 == 0 then "Fizz" else show x
+
 main :: IO ()
 main = hspec $ do
   describe "Example test" $ do
@@ -10,3 +13,13 @@ main = hspec $ do
       add 1 1 `shouldBe` 2
     it "2 + 2 = 4" $ do
       (2 + 2) `shouldBe` 4
+  describe "Fizz Buzz" $ do
+    it "1を入力すると1が返る" $ do
+      fizzBuzz 1 `shouldBe` "1"
+    it "2を入力すると2が返る" $ do
+      fizzBuzz 2 `shouldBe` "2"
+    it "3を入力するとfizzが返る" $ do
+      fizzBuzz 3 `shouldBe` "Fizz"
+    it "6を入力するとfizzが返る" $ do
+      fizzBuzz 6 `shouldBe` "Fizz"
+      
