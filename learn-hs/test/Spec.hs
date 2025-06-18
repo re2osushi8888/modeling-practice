cbdf816aa264c1edd0aa49bd4d6659cef82d33c5
Ryusei Ohkura <ohkura.exe@gmail.com>
2025-06-18 12:49:48 +0000

    FizzBuzzの実装とLoopの実装

--- learn-hs/test/Spec.hs
+++ learn-hs/test/Spec.hs
@@ -3,8 +3,19 @@
 import Lib (add)
 import Test.Hspec
 
+fizzBuzzLoop :: Integer -> [String]
+fizzBuzzLoop x = map fizzBuzz (enumFromTo 1 x)
+
 fizzBuzz :: Integer -> String
-fizzBuzz x = if mod x 3 == 0 then "Fizz" else show x
+fizzBuzz x = 
+  if mod x 15 == 0
+    then "FizzBuzz"
+  else if mod x 3 == 0 
+    then "Fizz" 
+  else if mod x 5 == 0 
+    then "Buzz" 
+  else
+    show x
 
 main :: IO ()
 main = hspec $ do
@@ -22,4 +33,15 @@ main = hspec $ do
       fizzBuzz 3 `shouldBe` "Fizz"
     it "6を入力するとfizzが返る" $ do
       fizzBuzz 6 `shouldBe` "Fizz"
-      
+    it "5を入力するとBuzzが返る" $ do
+      fizzBuzz 5 `shouldBe` "Buzz"
+    it "15を入力するとFizzBuzzが返る" $ do
+      fizzBuzz 15 `shouldBe` "FizzBuzz"
+
+  describe "FizzBuzzLoop" $ do
+    it "1を入力すると[1]が返る" $ do
+      fizzBuzzLoop 1 `shouldBe` ["1"]
+    it "3を入れると[1,2,Fizz]が返る" $ do
+      fizzBuzzLoop 3 `shouldBe` ["1", "2", "Fizz"]
+    it "15を入れても正しい値が返る" $ do
+      fizzBuzzLoop 15 `shouldBe` ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
